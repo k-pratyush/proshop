81e98db2c7d12658f4442fc52b5ea32a94cf1407
Pratyush Kerhalkar <pratyushkerhalkar@gmail.com>
2021-01-10 13:21:35 +0530

    user auth frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import { Container } from 'react-bootstrap';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
   return (
@@ -13,9 +14,10 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
